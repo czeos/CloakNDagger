e8d6f3211b53393f2429afe582e510ce4676d5e2
micha <michal.opletal@gisat.cz>
2024-09-25 13:20:26 +0200

    patch Regiter

--- transforms/CND_GetEntity.py
+++ transforms/CND_GetEntity.py
@@ -3,20 +3,20 @@ from maltego_trx.maltego import MaltegoTransform, MaltegoMsg
 
 from extensions import registry
 from settings import cloak_n_dagger_transformset
-from tools.maltego import create_entity_from_model, model_from_maltego_request
+from tools.maltego import entity_from_model, model_from_maltego_request
 from tools.entities import ENTITYREG, CloakNDagger
 from modules.cloak_n_dagger.gui import check_box_form
 
 
-starting_entities = [ENTITYREG.CASE.get_type(),
-                    ENTITYREG.ALIAS.get_type(),
-                    ENTITYREG.TAVILY.get_type()]
+starting_entities = {'Hunchly Case': ENTITYREG.CASE,
+                    'Whatsmyname App': ENTITYREG.ALIAS,
+                    'Tavily search': ENTITYREG.TAVILY}
 
 @registry.register_transform(
     display_name="Get CND strarting Entity [CloakNDagger]",
     input_entity=ENTITYREG.CLOAK_N_DAGGER.get_type(),
     description="Get starting entity",
-    output_entities=[name for name in starting_entities],
+    output_entities=[entity.get_type() for entity in starting_entities.values()],
     transform_set=cloak_n_dagger_transformset
 
 )
@@ -28,17 +28,16 @@ class CND_GetEntity(DiscoverableTransform):
     def create_entities(cls, request: MaltegoMsg, response: MaltegoTransform):
         # build query
         _ = model_from_maltego_request(request=request, model=CloakNDagger)
-        selected_entities = check_box_form(options=starting_entities,
+        selected_entities = check_box_form(options=[name for name in starting_entities.keys()],
                                         title='CloakNDagger',
                                         description='Select main project entities')
 
 
-        items = [ENTITYREG.get_cls(name)() for name in selected_entities]
+        items = [starting_entities.get(name).get_cls()() for name in selected_entities]
 
         # generating of pages
         for item in items:
-            create_entity_from_model(item, response)
+            entity_from_model(item, response)
 
         response.addUIMessage(f"{len(selected_entities)} were created")
-
 
